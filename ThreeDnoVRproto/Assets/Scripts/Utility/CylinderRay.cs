51d90419331918ab7ccc94128c9a90d756e7ce94
SpeakingCraniums <wesley.weissenberger@gmail.com>
2017-03-08 22:48:01 -0500

    Did some fooling with Raycasts
    and I didnt break anything so drunk is empty

---
+++ ThreeDnoVRproto/Assets/Scripts/Utility/CylinderRay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderRay : MonoBehaviour {
+
+	private RaycastHit hitInfo;
+	private Ray myRay;
+	private Vector3 shootOffset;
+	private Quaternion rotOffset;
+	public Planet thisPlanet;
+
+	void Start(){
+		shootOffset = new Vector3(-5,0,0);
+		print(transform.position);
+		print(thisPlanet.transform.position);
+		shootOffset = (transform.position - thisPlanet.transform.position).normalized;
+		print(shootOffset);
+		rotOffset = Quaternion.LookRotation(shootOffset);
+		print(rotOffset);
+		Debug.DrawRay(transform.position-shootOffset,shootOffset, Color.cyan);
+	}
+	void Update () {
+		myRay = new Ray(transform.position-shootOffset,shootOffset);	
+		Debug.DrawRay(transform.position-shootOffset,shootOffset, Color.cyan);
+	}
+
+	void FixedUpdate(){
+		if (Physics.Raycast(myRay, out hitInfo))
+			print("OHHHH SHIT" + hitInfo.distance);
+	}
+}
