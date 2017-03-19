91b2241902784283c7c88f37f5b9cd34714bc60b
SpeakingCraniums <wesley.weissenberger@gmail.com>
2017-03-19 19:43:45 -0400

    nurbs
    the burbs

--- ThreeDnoVRproto/Assets/Scripts/Utility/CylinderRay.cs
+++ ThreeDnoVRproto/Assets/Scripts/Utility/CylinderRay.cs
@@ -16,14 +16,8 @@ public class CylinderRay : MonoBehaviour {
 
 	void FixedUpdate(){
 		if (Physics.Raycast(myRay, out hitInfo)){
-			Vector3 pointHit = hitInfo.point;
-			pointHit -= GetComponent<CylinderRay>().gameObject.transform.position;
-			// just convert to a vector2
-			Vector2 v2HitPoint = (Vector2)pointHit;
-
-			// remember that this is just the X and Y values relative to spheres centre point.
-			Debug.Log("V2 local position hit : " + v2HitPoint);
+			Vector3 pointHit = hitInfo.textureCoord2;
 			Debug.Log("also printing :" + pointHit);
+		}
 	}
-}
 }
